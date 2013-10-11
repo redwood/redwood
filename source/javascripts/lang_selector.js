e5658f498a1e42c154a4c5b78a767194f89d595d
Robert Lord <robert@lord.io>
2013-10-11 11:55:42 -0700

    Update copyright year

--- source/javascripts/lang_selector.js
+++ source/javascripts/lang_selector.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2008-2012 Concur Technologies, Inc.
+Copyright 2008-2013 Concur Technologies, Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License"); you may
 not use this file except in compliance with the License. You may obtain
