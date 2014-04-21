6d73ffb05284b091c96911c515524ed56ef2ea71
Robert Lord <robert@lord.io>
2014-04-21 11:56:00 -0400

    Remove leftover console.logs from tocify edits

--- source/javascripts/lib/jquery.tocify.js
+++ source/javascripts/lib/jquery.tocify.js
@@ -595,9 +595,7 @@
             // Reset height cache on scroll
 
             $(window).on('resize', function() {
-                console.log("resizing" + self.cachedHeights);
                 self.calculateHeights();
-                console.log("done" + self.cachedHeights);
             });
 
             // Window scroll event handler
