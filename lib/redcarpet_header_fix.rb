052036b0c295739e6fa53b4ce2bcdd467803ca25
Robert Lord <robert@lord.io>
2014-07-09 22:36:44 -0700

    Fix incorrect header nesting in redcarpet patch
    Spotted by @moizjv, thanks!

--- lib/redcarpet_header_fix.rb
+++ lib/redcarpet_header_fix.rb
@@ -1,7 +1,7 @@
 module RedcarpetHeaderFix
   def header(text, level, id)
     clean_id = id.gsub(/[\.]/, '-').gsub(/[^a-zA-Z0-9\-_]/, '')
-    "<h#{level} id='#{clean_id}'>#{text}</h1>"
+    "<h#{level} id='#{clean_id}'>#{text}</h#{level}>"
   end
 end
 
