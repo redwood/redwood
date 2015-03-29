5da237e7fe9d7bfa654d5f81aabd7a79c997d8c9
Rouven Weßling <me@rouvenwessling.de>
2015-03-29 22:39:41 +0200

    Don't use a selector to get the heading text for search results.
    Prevents bugs with special CSS characters.
    
    Closes #201.

--- source/javascripts/app/search.js
+++ source/javascripts/app/search.js
@@ -50,7 +50,8 @@
       if (results.length) {
         searchResults.empty();
         $.each(results, function (index, result) {
-          searchResults.append("<li><a href='#" + result.ref + "'>" + $('#'+result.ref).text() + "</a></li>");
+          var elem = document.getElementById(result.ref);
+          searchResults.append("<li><a href='#" + result.ref + "'>" + $(elem).text() + "</a></li>");
         });
         highlight.call(this);
       } else {
