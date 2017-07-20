a00b046b3d43cc2f3fa8ff436f376b57bccdac4d
Diego <Diegomendez-0037@hotmail.com>
2017-07-20 11:50:47 -0600

    Update_lang.js
    on line 101 missing a semicolon

--- source/javascripts/app/_lang.js
+++ source/javascripts/app/_lang.js
@@ -98,7 +98,7 @@ under the License.
   // gets the language set in the query string
   function getLanguageFromQueryString() {
     if (location.search.length >= 1) {
-      var language = parseURL(location.search).language
+      var language = parseURL(location.search).language;
       if (language) {
         return language;
       } else if (jQuery.inArray(location.search.substr(1), languages) != -1) {
