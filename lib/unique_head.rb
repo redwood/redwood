48807d74c9198101e7d55c7ea730e20037f318c5
Robert Lord <robert@lord.io>
2017-11-23 10:40:00 -0800

    Fix #837 Chinese, Russian headers, thx finalnova

--- lib/unique_head.rb
+++ lib/unique_head.rb
@@ -1,5 +1,6 @@
 # Unique header generation
 require 'middleman-core/renderers/redcarpet'
+require 'digest'
 class UniqueHeadCounter < Middleman::Renderers::MiddlemanRedcarpetHTML
   def initialize
     super
@@ -7,6 +8,12 @@ class UniqueHeadCounter < Middleman::Renderers::MiddlemanRedcarpetHTML
   end
   def header(text, header_level)
     friendly_text = text.gsub(/<[^<]+>/,"").parameterize
+    if friendly_text.strip.length == 0
+      # Looks like parameterize removed the whole thing! It removes many unicode
+      # characters like Chinese and Russian. To get a unique URL, let's just
+      # URI escape the whole header
+      friendly_text = Digest::SHA1.hexdigest(text)[0,10]
+    end
     @head_count[friendly_text] ||= 0
     @head_count[friendly_text] += 1
     if @head_count[friendly_text] > 1
