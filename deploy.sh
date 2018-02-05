603d4cd797abc76e09962bb8c4818a278fcc2fb2
Andrey Fedorov <me@anfedorov.com>
2018-02-05 10:51:09 -0800

    Rename OS X to macOS in deploy.sh comment (#951)

--- deploy.sh
+++ deploy.sh
@@ -102,7 +102,7 @@ main() {
     return 1
   fi
 
-  # must use short form of flag in ls for compatibility with OS X and BSD
+  # must use short form of flag in ls for compatibility with macOS and BSD
   if [[ -z `ls -A "$deploy_directory" 2> /dev/null` && -z $allow_empty ]]; then
     echo "Deploy directory '$deploy_directory' is empty. Aborting. If you're sure you want to deploy an empty tree, use the --allow-empty / -e flag." >&2
     return 1
