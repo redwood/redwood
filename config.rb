36b7e4430d81ddfd34bf4ed5bc1f571382e1c206
Sam Gilman <thenengah@gmail.com>
2019-06-02 05:36:44 +0800

    updated config.rb to activate asset_hash (#1076)

--- config.rb
+++ config.rb
@@ -40,6 +40,7 @@ set :relative_links, true
 
 # Build Configuration
 configure :build do
+  activate :asset_hash
   # If you're having trouble with Middleman hanging, commenting
   # out the following two lines has been known to help
   activate :minify_css
