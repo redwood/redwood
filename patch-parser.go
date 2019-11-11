e4a54b976286d8db45b3b11ac34cb2d1ec5c3aeb
Bryn Bellomy <bryn.bellomy@gmail.com>
2019-11-10 20:29:52 -0600

    Rewrite patch parser from scratch

--- patch-parser.go
+++ patch-parser.go
@@ -2,96 +2,124 @@ package redwood
 
 import (
 	"encoding/json"
-	"strings"
+	"strconv"
 
-	"github.com/alecthomas/participle"
-	"github.com/alecthomas/participle/lexer"
 	"github.com/pkg/errors"
 )
 
-type parserPatch struct {
-	Keys   parserPatchKeys   `@@`
-	Range  *parserPatchRange `("[" @@ "]")?`
-	Equals parserPatchEquals `@@`
-}
-
-type parserPatchParts struct {
-}
-
-type parserPatchKeys struct {
-	Keys []string `("." @Ident | "[\" @Ident \"]")*`
-}
-
-type parserPatchRange struct {
-	Start int64  `@Int`
-	End   *int64 `(":" @Int)?`
-}
-
-type parserPatchEquals struct {
-	Pos    lexer.Position
-	Exists bool `@"="`
-}
+var ErrBadPatch = errors.New("bad patch string")
 
 func ParsePatch(s string) (Patch, error) {
-	idx := strings.Index(s, "=")
-	if idx == -1 {
-		return Patch{}, errors.New("wat")
-	}
+	patch := Patch{}
 
-	keypath := s[:idx]
-	jsonBlob := s[idx+1:]
+	for i := 0; i < len(s); {
+		switch s[i] {
+		case '.':
+			key, err := parseDotKey(s[i:])
+			if err != nil {
+				return Patch{}, err
+			}
+			patch.Keys = append(patch.Keys, key)
+			i += len(key) + 1
 
-	parser, err := participle.Build(&parserPatch{})
-	if err != nil {
-		return Patch{}, errors.WithStack(err)
-	}
+		case '[':
+			switch s[i+1] {
+			case '"', '\'':
+				key, err := parseBracketKey(s[i:])
+				if err != nil {
+					return Patch{}, err
+				}
+				patch.Keys = append(patch.Keys, key)
+				i += len(key) + 4
 
-	var p parserPatch
-	err = parser.ParseString(keypath, &p)
-	if err != nil {
-		// @@TODO: the fact that we don't try to consume the entire string makes the parser mad
-		// return Patch{}, err
-	}
+			case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+				rng, length, err := parseRange(s[i:])
+				if err != nil {
+					return Patch{}, err
+				}
+				patch.Range = rng
+				i += length
+			}
 
-	var rng *Range
-	if p.Range != nil {
-		end := p.Range.Start
-		if p.Range.End != nil {
-			end = *p.Range.End
+		case ' ', '=':
+			for s[i] != '=' {
+				i++
+			}
+			i++
+			err := json.Unmarshal([]byte(s[i:]), &patch.Val)
+			if err != nil {
+				return Patch{}, errors.Wrapf(ErrBadPatch, err.Error())
+			}
+			return patch, nil
 		}
-		rng = &Range{Start: p.Range.Start, End: end}
 	}
-
-	patch := Patch{
-		Keys:  p.Keys.Keys,
-		Range: rng,
-	}
-
-	jsonBlob = jsonEscape(jsonBlob)
-
-	err = json.Unmarshal([]byte(jsonBlob), &patch.Val)
-	if err != nil {
-		panic(err)
-		return Patch{}, errors.WithStack(err)
-	}
-
-	return patch, nil
+	return Patch{}, errors.WithStack(ErrBadPatch)
 }
 
-func jsonEscape(s string) string {
-	retval := ""
-
-	var inString bool
-	for i := 0; i < len(s); i++ {
-		if s[i] == '\n' && inString {
-			retval += "\\n"
-			continue
+func parseDotKey(s string) (string, error) {
+	buf := []byte{}
+	// start at index 1, skip first dot
+	for i := 1; i < len(s); i++ {
+		if s[i] == '.' || s[i] == '[' || s[i] == ' ' {
+			return string(buf), nil
+		} else {
+			buf = append(buf, s[i])
 		}
-
-		if s[i] == '"' {
-			inString = !inString
-		}
-		retval += string(s[i])
 	}
-	return retval
+	return "", errors.WithStack(ErrBadPatch)
+}
+
+func parseBracketKey(s string) (string, error) {
+	if len(s) < 5 {
+		return "", errors.WithStack(ErrBadPatch)
+	} else if s[0] != '[' && s[1] != '"' {
+		return "", errors.WithStack(ErrBadPatch)
+	}
+
+	buf := []byte{}
+	// start at index 2, skip ["
+	for i := 2; i < len(s); i++ {
+		if s[i] == '"' {
+			return string(buf), nil
+		} else {
+			buf = append(buf, s[i])
+		}
+	}
+	return "", errors.WithStack(ErrBadPatch)
+}
+
+func parseRange(s string) (*Range, int, error) {
+	rng := &Range{}
+	haveStart := false
+	buf := []byte{}
+	// start at index 1, skip [
+	for i := 1; i < len(s); i++ {
+		if s[i] == ']' {
+			if !haveStart {
+				return nil, 0, errors.WithStack(ErrBadPatch)
+			}
+			end, err := strconv.ParseInt(string(buf), 10, 64)
+			if err != nil {
+				return nil, 0, errors.WithStack(ErrBadPatch)
+			}
+			rng.End = end
+			return rng, i + 1, nil
+
+		} else if s[i] == ':' {
+			if haveStart {
+				return nil, 0, errors.WithStack(ErrBadPatch)
+			}
+
+			start, err := strconv.ParseInt(string(buf), 10, 64)
+			if err != nil {
+				return nil, 0, errors.WithStack(ErrBadPatch)
+			}
+			buf = []byte{}
+			rng.Start = start
+			haveStart = true
+		} else {
+			buf = append(buf, s[i])
+		}
+	}
+	return nil, 0, errors.WithStack(ErrBadPatch)
 }
