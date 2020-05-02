fb9ee72fba819a84f1915d3f02c64f9dffb8373d
Bryn Bellomy <bryn.bellomy@gmail.com>
2020-05-01 19:23:30 -0500

    tree: add more test fixtures, clean up fixture code

--- tree/fixtures_test.go
+++ tree/fixtures_test.go
@@ -21,11 +21,14 @@ type fixtureOutput struct {
 	value    interface{}
 }
 
+type M = map[string]interface{}
+type S = []interface{}
+
 var (
 	testVal1 = M{
-		"asdf": []interface{}{"1234", float64(987.2), uint64(333)},
+		"asdf": S{"1234", float64(987.2), uint64(333)},
 		"flo":  float64(321),
-		"flox": []interface{}{
+		"flox": S{
 			uint64(65),
 			M{"yup": "yes", "hey": uint64(321)},
 			"jkjkjkj",
@@ -37,12 +40,12 @@ var (
 	}
 	testVal1Output = []fixtureOutput{
 		{Keypath(nil), NodeTypeMap, testVal1},
-		{Keypath("asdf"), NodeTypeSlice, []interface{}{"1234", float64(987.2), uint64(333)}},
+		{Keypath("asdf"), NodeTypeSlice, S{"1234", float64(987.2), uint64(333)}},
 		{Keypath("asdf").Push(EncodeSliceIndex(0)), NodeTypeValue, "1234"},
 		{Keypath("asdf").Push(EncodeSliceIndex(1)), NodeTypeValue, float64(987.2)},
 		{Keypath("asdf").Push(EncodeSliceIndex(2)), NodeTypeValue, uint64(333)},
 		{Keypath("flo"), NodeTypeValue, float64(321)},
-		{Keypath("flox"), NodeTypeSlice, []interface{}{uint64(65), M{"yup": "yes", "hey": uint64(321)}, "jkjkjkj"}},
+		{Keypath("flox"), NodeTypeSlice, S{uint64(65), M{"yup": "yes", "hey": uint64(321)}, "jkjkjkj"}},
 		{Keypath("flox").Push(EncodeSliceIndex(0)), NodeTypeValue, uint64(65)},
 		{Keypath("flox").Push(EncodeSliceIndex(1)), NodeTypeMap, M{"yup": "yes", "hey": uint64(321)}},
 		{Keypath("flox").Push(EncodeSliceIndex(1)).Push(Keypath("hey")), NodeTypeValue, uint64(321)},
@@ -56,34 +59,46 @@ var (
 
 	testVal2 = M{
 		"ooooooo": uint64(332211),
-		"eee": []interface{}{
+		"eee": S{
 			M{
-				"qqqq": []interface{}{"fjfjjfjf", uint64(123321)},
+				"qqqq": S{"fjfjjfjf", uint64(123321)},
 			},
 		},
 	}
 	testVal2Output = []fixtureOutput{
 		{Keypath(nil), NodeTypeMap, testVal2},
-		{Keypath("eee"), NodeTypeSlice, []interface{}{M{"qqqq": []interface{}{"fjfjjfjf", uint64(123321)}}}},
-		{Keypath("eee").Push(EncodeSliceIndex(0)), NodeTypeMap, M{"qqqq": []interface{}{"fjfjjfjf", uint64(123321)}}},
-		{Keypath("eee").Push(EncodeSliceIndex(0)).Push(Keypath("qqqq")), NodeTypeSlice, []interface{}{"fjfjjfjf", uint64(123321)}},
+		{Keypath("eee"), NodeTypeSlice, S{M{"qqqq": S{"fjfjjfjf", uint64(123321)}}}},
+		{Keypath("eee").Push(EncodeSliceIndex(0)), NodeTypeMap, M{"qqqq": S{"fjfjjfjf", uint64(123321)}}},
+		{Keypath("eee").Push(EncodeSliceIndex(0)).Push(Keypath("qqqq")), NodeTypeSlice, S{"fjfjjfjf", uint64(123321)}},
 		{Keypath("eee").Push(EncodeSliceIndex(0)).Push(Keypath("qqqq")).Push(EncodeSliceIndex(0)), NodeTypeValue, "fjfjjfjf"},
 		{Keypath("eee").Push(EncodeSliceIndex(0)).Push(Keypath("qqqq")).Push(EncodeSliceIndex(1)), NodeTypeValue, uint64(123321)},
 		{Keypath("ooooooo"), NodeTypeValue, uint64(332211)},
 	}
 	fixture2 = fixture{testVal2, testVal2Output}
 
-	testVal3 = []interface{}{
+	testVal3 = S{
 		uint64(8383),
 		M{"9999": "hi", "vvvv": "yeah"},
 		float64(321.23),
+		"hello",
 	}
+	testVal3Output = []fixtureOutput{
+		{Keypath(nil), NodeTypeSlice, testVal3},
+		{Keypath(EncodeSliceIndex(0)), NodeTypeValue, uint64(8383)},
+		{Keypath(EncodeSliceIndex(1)), NodeTypeMap, M{"9999": "hi", "vvvv": "yeah"}},
+		{Keypath(EncodeSliceIndex(1)).Push(Keypath("9999")), NodeTypeValue, "hi"},
+		{Keypath(EncodeSliceIndex(1)).Push(Keypath("vvvv")), NodeTypeValue, "yeah"},
+		{Keypath(EncodeSliceIndex(2)), NodeTypeValue, float64(321.23)},
+		{Keypath(EncodeSliceIndex(3)), NodeTypeValue, "hello"},
+	}
+	fixture3 = fixture{testVal3, testVal3Output}
+
 	testVal4 = M{
 		"lkjlkj": M{
 			"aaa": float64(33),
-			"bbb": []interface{}{true, uint64(234), "fjeijfiejf", float64(321)},
+			"bbb": S{true, uint64(234), "fjeijfiejf", float64(321)},
 		},
-		"zzzzz": []interface{}{
+		"zzzzz": S{
 			M{"eee": "fff", "ggg": false},
 			M{"hhh": true},
 			true,
@@ -102,7 +117,7 @@ var (
 	testVal7Output = []fixtureOutput{{Keypath(nil), NodeTypeValue, testVal7}}
 	fixture7       = fixture{testVal7, testVal7Output}
 
-	testVal8       = []interface{}{}
+	testVal8       = S{}
 	testVal8Output = []fixtureOutput{{Keypath(nil), NodeTypeSlice, testVal8}}
 	fixture8       = fixture{testVal8, testVal8Output}
 
@@ -137,9 +152,13 @@ func combineFixtureOutputs(keypathPrefix Keypath, fixtures ...fixture) []fixture
 }
 
 func makeAtKeypathFixtureOutputs(atKeypath Keypath) []fixtureOutput {
+	if atKeypath.NumParts() == 0 {
+		return nil
+	}
 	var current Keypath
 	var outs []fixtureOutput
 	atKeypathParts := append([]Keypath{nil}, atKeypath.Parts()...)
+	atKeypathParts = atKeypathParts[:len(atKeypathParts)-1] // Remove the last item -- it's added by the test fixture
 	for _, part := range atKeypathParts {
 		current = current.Push(part)
 		outs = append(outs, fixtureOutput{
