f390aba9a2d296da812d789ff054094bdf108b71
Andrew Lloyd Cartwright <andrewlloydcartwright+atlc@gmail.com>
2020-06-21 17:28:18 -0500

    Strip HTML tags from ToC element with regex before showing in title bar (#1120)

--- source/javascripts/app/_toc.js
+++ source/javascripts/app/_toc.js
@@ -80,9 +80,9 @@
         if (window.history.replaceState) {
           window.history.replaceState(null, "", best);
         }
-        var thisTitle = $best.data("title")
+        var thisTitle = $best.data("title");
         if (thisTitle !== undefined && thisTitle.length > 0) {
-          document.title = thisTitle + " – " + originalTitle;
+          document.title = thisTitle.replace(htmlPattern, "") + " – " + originalTitle;
         } else {
           document.title = originalTitle;
         }
