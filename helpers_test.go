ca5526c286cf3f4c252f23c046171a3fd7e17cb8
Bryn Bellomy <bryn@signals.io>
2021-03-21 15:56:13 -0500

    Add peer pool tests

--- helpers_test.go
+++ helpers_test.go
@@ -1,12 +1,47 @@
 package redwood
 
 import (
+	"fmt"
+	"strings"
 	"time"
 
 	"redwood.dev/crypto"
 	"redwood.dev/types"
 	"redwood.dev/utils"
 )
+
+var NewPeerPool = newPeerPool
+
+const (
+	PeerState_Unknown = peerState_Unknown
+	PeerState_Strike  = peerState_Strike
+	PeerState_InUse   = peerState_InUse
+)
+
+func (entry peersMapEntry) State() peerState {
+	return entry.state
+}
+
+func (p *peerPool) PrintPeers(label string) map[PeerDialInfo]peersMapEntry {
+	peers := p.CopyPeers()
+	lines := []string{label + " ======================"}
+	for key, val := range peers {
+		lines = append(lines, fmt.Sprintf("  - %v %v", key, val))
+	}
+	lines = append(lines, "=======================")
+	fmt.Println(strings.Join(lines, "\n"))
+	return peers
+}
+
+func (p *peerPool) CopyPeers() map[PeerDialInfo]peersMapEntry {
+	p.peersMu.Lock()
+	defer p.peersMu.Unlock()
+	m := make(map[PeerDialInfo]peersMapEntry)
+	for dialInfo, entry := range p.peers {
+		m[dialInfo] = entry
+	}
+	return m
+}
 
 func (p *peerStore) FetchAllPeerDetails() ([]*peerDetails, error) {
 	return p.fetchAllPeerDetails()
