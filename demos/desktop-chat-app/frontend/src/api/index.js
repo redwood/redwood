0974100e9cf38fff97f112151e65c691112b2f6d
Bryn Bellomy <bryn@signals.io>
2021-04-07 17:22:12 -0500

    desktop-chat-app: Fix rpc addPeer method

--- demos/desktop-chat-app/frontend/src/api/index.js
+++ demos/desktop-chat-app/frontend/src/api/index.js
@@ -4,7 +4,7 @@ const sync9JSSha3 = '0c87e1035db28f334cd7484b47d9e7cc285e026d4f876d24ddad78c47ac
 
 export default function(redwoodClient, ownAddress) {
     async function addPeer(transportName, dialAddr) {
-        await redwoodClient.rpc.addPeer({ TransportName: transportName, DialAddr: dialAddr })
+        await redwoodClient.rpc.addPeer({ transportName, dialAddr })
     }
 
     async function addServer(server, iconFile, provider, cloudStackOptions) {
@@ -255,6 +255,7 @@ export default function(redwoodClient, ownAddress) {
     }
 
     return {
+        addPeer,
         addServer,
         importServer,
         subscribe,
