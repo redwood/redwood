499f384ecb18a7b94f003b57379b711394470900
Bryn Bellomy <bryn@signals.io>
2021-05-07 17:44:34 -0500

    types: remove ChallengeMsg, add ErrUnsupported

--- types/types.go
+++ types/types.go
@@ -171,27 +171,6 @@ func (sig Signature) Copy() Signature {
 	return cp
 }
 
-type ChallengeMsg []byte
-
-func GenerateChallengeMsg() ([]byte, error) {
-	challengeMsg := make([]byte, 128)
-	_, err := rand.Read(challengeMsg)
-	return challengeMsg, err
-}
-
-func (c ChallengeMsg) MarshalJSON() ([]byte, error) {
-	return []byte(`"` + hex.EncodeToString(c) + `"`), nil
-}
-
-func (c *ChallengeMsg) UnmarshalJSON(bs []byte) error {
-	bs, err := hex.DecodeString(string(bs[1 : len(bs)-1]))
-	if err != nil {
-		return errors.WithStack(err)
-	}
-	*c = bs
-	return nil
-}
-
 type Hash [32]byte
 
 var (
