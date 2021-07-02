e13c3e4f03cb5caec56b0ea2c26df5438fe1e29d
Bryn Bellomy <bryn@signals.io>
2021-07-02 14:20:21 -0500

    Regenerate protobufs

--- internal/pb/gen.go
+++ internal/pb/gen.go
@@ -1,3 +1,3 @@
 package pb
 
-//go:generate protoc -I . tx.proto --go_out=.
+//go:generate protoc -I=. -I=$GOPATH/src -I=$GOPATH/src/github.com/gogo/protobuf/protobuf --gogoslick_out=Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types:. tx.proto
