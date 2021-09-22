847435c3b28edf87795d17fae0361606dc6ca7d5
Bryn Bellomy <bryn@signals.io>
2021-09-22 18:57:24 -0500

    Add useTraceUpdate hook

---
+++ demos/desktop-chat-app/frontend/src/hooks/useTraceUpdate.js
@@ -0,0 +1,19 @@
+import { useEffect, useRef } from 'react'
+
+function useTraceUpdate(props) {
+    const prev = useRef(props)
+    useEffect(() => {
+        const changedProps = Object.entries(props).reduce((ps, [k, v]) => {
+            if (prev.current[k] !== v) {
+                ps[k] = [prev.current[k], v]
+            }
+            return ps
+        }, {})
+        if (Object.keys(changedProps).length > 0) {
+            console.log('Changed props:', changedProps)
+        }
+        prev.current = props
+    })
+}
+
+export default useTraceUpdate
